7fc5c4d96290321c94c5437ce9e5279e6cd373f7
jlarge1 <largejonathan@gmail.com>
2018-05-25 03:11:52 -0400

    initial work

---
+++ assets/javascript/app.js
@@ -0,0 +1,66 @@
+
+var animals = ["dolphin", "guinea pig", "parakeet", "racoon", "cow", "puppy", "cat", "lion", "falcon", "panda", "eagle", "otter", "penguin", "wolf", "duck", "cricket", "ferret", "iguana", "chicken", "iguana"]
+var still = [];
+var moving = [];
+
+function displayGifs() {
+    var searchTerm = $(this).attr("data-name");
+    var apiKey = "bw79f5nB2UMqatnUhZCZZ2PYtuvmRjMf"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchTerm + "&limit=10&offset=0&rating=PG-13&lang=en";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.data[0].images.downsized_still.url);
+        $("#top").empty();
+        
+
+        for (i = 0; i < 10; i++) {
+            var newCard = $("<div>").addClass("gifDisplay");
+            newCard.append("<img src=" + response.data[i].images.fixed_height_still.url + ' class="gif" imageNum="1" id="' + i + '">');
+            $("#top").append(newCard);
+            console.log("should have cards");
+        };
+
+        $(document).on("click", ".gif", function () {
+            var item = $(this).attr("id");
+            var imageNum = $(this).attr("imageNum");
+            if (imageNum == 1) {
+                $(this).attr("src", response.data[item].images.fixed_height.url);
+                $(this).attr("imageNum", 0);
+            } 
+            if (imageNum == 0) {
+                $(this).attr("src", response.data[item].images.fixed_height_still.url);
+                $(this).attr("imageNum", 1); 
+            }
+            
+        });
+
+    });
+}
+
+function createButtons() {
+    $("#buttons").empty();
+
+    $.each(animals, function (i, val) {
+        var button = $("<button>").text(val).addClass("button").attr("data-name", val);
+        $("#buttons").prepend(button);
+    });
+}
+
+
+
+
+createButtons();
+
+$("#add-searchTerm").on("click", function (event) {
+    event.preventDefault();
+    var animal = $("#searchTerm-input").val().trim();
+    animals.push(animal);
+
+    createButtons();
+});
+
+$(document).on("click", ".button", displayGifs);
